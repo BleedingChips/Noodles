5ff33c8daed103c4b965059615c4cfe181b7aa11
= <chips_code@outlook.com>
2022-04-29 19:30:28 +0800

    开始重构
    1. 重整内存池相关功能

--- Private/NoodlesMemory.cpp
+++ Private/NoodlesMemory.cpp
@@ -1,1 +1,48 @@
 #include "../Public/NoodlesMemory.h"
+
+
+namespace Noodles::PageMemory
+{
+	// 暂时先不管内存分配的，以后慢慢调优
+
+	void PageWrapper::AddRef(Page* Ptr)
+	{
+		assert(Ptr != nullptr);
+		Ptr->ReferenceCount.AddRef();
+	}
+
+	void PageWrapper::SubRef(Page* Ptr)
+	{
+		assert(Ptr != nullptr);
+		if (Ptr->ReferenceCount.SubRef())
+		{
+			Ptr->~Page();
+			delete Ptr;
+		}
+	}
+
+	void PageWrapper::AddRef(SubPage* Ptr)
+	{
+		assert(Ptr != nullptr);
+		Ptr->ReferenceCount.AddRef();
+	}
+
+	void PageWrapper::SubRef(SubPage* Ptr)
+	{
+		assert(Ptr != nullptr);
+		if (Ptr->ReferenceCount.SubRef())
+		{
+			Ptr->PagePtr.Reset();
+		}
+	}
+
+	SubPage ChunkAllocator::Allocator(std::size_t MinChunkSize, std::size_t Aligna)
+	{
+		std::size_t Size = sizeof(SubPage) + sizeof(Page);
+		if(Aligna > alignof(Size))
+			Size += Aligna - alignof(Size);
+		std::byte* Data = new std::byte[Size];
+		auto PagePtr = new (Data) Page;
+
+	}
+}
--- Public/NoodlesMemory.h
+++ Public/NoodlesMemory.h
@@ -1,13 +1,64 @@
 #pragma once
 #include <atomic>
 #include <array>
-namespace Noodle
+#include "Potato/Public/PotatoMisc.h"
+#include "Potato/Public/PotatoIntrusivePointer.h"
+
+namespace Noodles::PageMemory
 {
-	struct MemoryPageDescription
+
+	struct Allocator;
+
+	class Page;
+	class SubPage;
+
+	struct PageWrapper
 	{
-		MemoryPageDescription* FrontPage;
-		MemoryPageDescription* LastPage;
-		std::byte* StartEntityPtr;
-		std::byte* EndEntityPtr;
+		static void AddRef(Page* Ptr);
+		static void SubRef(Page* Ptr);
+		static void AddRef(SubPage* Ptr);
+		static void SubRef(SubPage* Ptr);
 	};
+
+	using PagePtr = Potato::Misc::IntrusivePtr<Page, PageWrapper>;
+	using SubPagePtr = Potato::Misc::IntrusivePtr<SubPage, PageWrapper>;
+
+	// 大页
+	class Page
+	{
+		mutable Potato::Misc::AtomicRefCount ReferenceCount;
+		std::span<SubPage> Pages; 
+		friend class MemoryPageWrapper;
+		friend class MemoryAllocator;
+		friend class MemorySubPage;
+
+		friend struct PageWrapper;
+		friend struct Allocator;
+	};
+
+	// 子页，小页属于大页的一部分，一般一个大页由很多个小页组成
+	class SubPage
+	{
+		std::span<std::byte> GetChunkData() { return Chunk; }
+	public:
+		mutable Potato::Misc::AtomicRefCount ReferenceCount;
+		std::atomic_bool IsUsing;
+		PagePtr PagePtr;
+		std::span<std::byte> Chunk;
+
+		friend struct PageWrapper;
+		friend struct Allocator;
+	};
+
+	
+
+	// 块状内存分配器
+	struct ChunkAllocator
+	{
+		SubPage Allocator(std::size_t MinChunkSize, std::size_t Aligna);
+	private:
+	};
+
+
+	
 }
